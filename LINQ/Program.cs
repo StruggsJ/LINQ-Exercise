fbf51a8ae4d6c1aaf8a12945222f0394bac273f7
StruggsJ <strjac5@gmail.com>
2023-09-13 16:26:35 -0500

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create a list.
+            
+            
+            
+            List<string> computerBrands = new List<string>()
+            { "MSI", "Gateway", "Dell", "HP", "Apple","Microsoft", "Asus", "IBM", "Toshiba", "Acer", "Intel", "Samsung", "Alienware"};
+
+            //Order the list by the length of the name, using method syntax and the lambda expression.
+
+          
+            IEnumerable<string> orderedLength =
+               computerBrands.OrderBy(brand => brand.Length);
+
+            foreach (string brand in orderedLength)
+            {
+                Console.WriteLine(brand);
+            }
+
+
+
+            
+            
+
+
         }
     }
 }
